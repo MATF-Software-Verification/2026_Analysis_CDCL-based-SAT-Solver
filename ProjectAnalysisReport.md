8c09aafda870ff52b078f2a994719e1619f47cf4
Staša Đorđević <stasadjordjevic2@gmail.com>
2026-02-05 15:33:44 +0100

    Add integration tests

--- ProjectAnalysisReport.md
+++ ProjectAnalysisReport.md
@@ -5,3 +5,47 @@ Sprovodi se analiza projekta koji je napisan u Python-u za rešavanje SAT proble
 
 Primenjeni su sledeći alati za analizu projekta:
 ...
+
+## Integraciono testiranje
+
+Za proveru ispravnosti solver-a sprovedeno je integraciono testiranje.  
+Testovi su smešteni u folderu `tests/integration` i sadrže formule u DIMACS CNF formatu.  
+Svaki test proverava da li je data formula zadovoljiva (SAT) ili nezadovoljiva (UNSAT).  
+
+Test primeri u folderu `/all_sat` preuzeti su sa sajta:  
+https://www.cs.ubc.ca/~hoos/SATLIB/benchm.html
+
+Integracionim testovima pokriveni su sledeći slučajevi:
+TODO DODATI JOS TESTOVA
+- Obične zadovoljive formule (SAT)
+- Obične nezadovoljive formule (UNSAT)
+- Specijalni slučajevi:
+  - Prazna formula
+  - Prazna klauza
+  - Formule sa jednom promenljivom
+
+Pokretanje integracionih testova vrši se pomoću skripte `test_integration.py`, koja prolazi kroz sve test fajlove i pokreće solver nad njima.
+
+### Pokretanje testova
+
+Testovi se pokreću naredbom:
+
+```bash
+pytest test_integration.py
+```
+### Izlaz testova
+
+Nakon pokretanja testova, `pytest` prikazuje rezultate izvršavanja, uključujući:
+
+- broj uspešno izvršenih testova,
+- eventualne greške,
+- vreme izvršavanja.
+
+Ovaj pristup omogućava jednostavnu reprodukciju testova i proveru funkcionalnosti kompletnog sistema.
+
+## Jedinično testiranje
+
+Jedinično testiranje ima za cilj proveru ispravnosti pojedinačnih komponenti rešavača izolovano od ostatka sistema.  
+Ovaj tip testiranja omogućava rano otkrivanje grešaka i olakšava održavanje i dalji razvoj koda.
+
+Jedinični testovi obuhvatiće proveru ispravnosti ...
---
+++ tests/integration/test_integration.py
@@ -0,0 +1,52 @@
+import pytest
+import sys
+import os
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../CDCL-based-SAT-Solver"))
+sys.path.insert(0, project_root)
+
+from cdcl_solver import CDCL_Solver
+all_sat_folder = os.path.join(os.path.dirname(__file__), "all_sat")
+# all_unsat_folder = os.path.join(os.path.dirname(__file__), "all_unsat")
+def test_all_sat():
+    for cnf_file in os.listdir(all_sat_folder):
+        if cnf_file.endswith(".cnf"):
+            file_path = os.path.join(all_sat_folder,cnf_file)
+            solver = CDCL_Solver(file_path,verbose=True)
+            result = solver.solve()() # pokrece solve koji vraca self.formula.get_value, pa uzimamo valuaciju formule 
+            print(f"File {cnf_file} solved with result: {result}")
+            assert result==1,f"File {cnf_file} expected SAT, got {result}"
+
+def test_unsat():
+    file_path = "unsat.cnf"
+    solver = CDCL_Solver(file_path,verbose=True)
+    result = solver.solve()()
+    print(f"File {file_path} solved with result: {result}")
+    assert result==-1,f"File {file_path} expected SAT, got {result}"
+
+# def test_empty_clause():
+#     file_path = "empty_clause.cnf"
+#     solver = CDCL_Solver(file_path,verbose=True)
+#     result = solver.solve()()
+#     print(f"File {file_path} solved with result: {result}")
+#     assert result==-1,f"File {file_path} expected SAT, got {result}"
+
+def test_empty_formula():
+    file_path = "empty_formula.cnf"
+    solver = CDCL_Solver(file_path,verbose=True)
+    result = solver.solve()()
+    print(f"File {file_path} solved with result: {result}")
+    assert result==1,f"File {file_path} expected SAT, got {result}"
+
+def test_one_var_sat():
+    file_path = "one_var_sat.cnf"
+    solver = CDCL_Solver(file_path,verbose=True)
+    result = solver.solve()()
+    print(f"File {file_path} solved with result: {result}")
+    assert result==1,f"File {file_path} expected SAT, got {result}"
+
+def test_one_var_unsat():
+    file_path = "one_var_unsat.cnf"
+    solver = CDCL_Solver(file_path,verbose=True)
+    result = solver.solve()()
+    print(f"File {file_path} solved with result: {result}")
+    assert result==-1,f"File {file_path} expected SAT, got {result}"
